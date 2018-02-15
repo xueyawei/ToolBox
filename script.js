500e15704627dad98631c1d9aecc433ceaf09871
xueyawei <153379052@qq.com>
2018-02-14 21:11:14 -0500

    add media type

--- script.js
+++ script.js
@@ -18,18 +18,35 @@ $("#submit").click(function (e) {
     var date = new Date($("#date").val());
     var link = $("#link").val();
     var text = $("#main-text").val()
+    var source = $("#source").val();
 
     console.log(title, date, link, text)
     
 
     var dateText = monthArray[date.getMonth()]+" "+date.getDate().toString()+", "+date.getFullYear().toString();
-    var outputText = "<div class='blog-card'>" +
+    
+    // get output text type;
+    var outputText;
+    var flag = $("#blog-type").prop("checked");
+    if(!flag){
+        outputText = "<div class='blog-card'>" +
+        "\r\t<h4><strong><em>"+source+"</em> | " + dateText + "</strong></h4>" +
+        "\r\t<h1>" + title + "</h1>" +
+        "\r\t" + text +
+        "..." +
+        "\r\t<a href='" + link + "'>Read More &gt;&gt;</a>" +
+        "\r</div>";
+    }else{
+        outputText = "<div class='blog-card'>" +
         "\r\t<h1>" + title + "</h1>" +
         "\r\t<h4><strong>" + dateText + "</strong></h4>" +
         "\r\t" + text +
         "..." +
         "\r\t<a href='" + link + "'>Read More &gt;&gt;</a>" +
-        "\r</div>"
+        "\r</div>";
+    }
+    
+    console.log(outputText);
     $('#output-text').val(outputText);
     
     var allText = $('#all').val();
@@ -48,3 +65,14 @@ $('#allClear').click(function(){
     $("#output-text").val("");
     $("#all").val("");
 });
+
+$("#media-type").on("click",function(){
+    if($("#media-type").prop("checked")){
+        $("#source").prop("disabled",false);
+    }
+   
+});
+
+$("#blog-type").click(function(){
+    $("#source").prop("disabled",true);
+})
